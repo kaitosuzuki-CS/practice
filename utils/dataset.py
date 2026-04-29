e0d6613e731ff8b1ecec85264eb640255d804837
kaito <szkkaito@gmail.com>
2026-04-29 13:34:06 -0700

    update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -8,30 +8,32 @@ from torchvision.datasets import MNIST
 parent_dir = Path(__file__).resolve().parent.parent
 
 
-def create_dataset(hps):
+def create_dataset(train_hps):
+    data_hps = train_hps.data
+
     transform = transforms.Compose(
         [
             transforms.ToTensor(),
-            transforms.Resize(tuple(hps.im_size)),
+            transforms.Resize(tuple(data_hps.im_size)),
             transforms.Normalize((0.5,), (0.5,)),
         ]
     )
 
-    data_dir = os.path.join(parent_dir, getattr(hps, "data_dir", "data"))
+    data_dir = os.path.join(parent_dir, getattr(data_hps, "data_dir", "data"))
 
     train_dataset = MNIST(root=data_dir, train=True, transform=transform, download=True)
     val_dataset = MNIST(root=data_dir, train=False, transform=transform, download=True)
 
     train_loader = DataLoader(
         train_dataset,
-        batch_size=int(hps.train_bs),
+        batch_size=int(data_hps.train_bs),
         shuffle=True,
         drop_last=True,
         num_workers=4,
     )
     val_loader = DataLoader(
         val_dataset,
-        batch_size=int(hps.val_bs),
+        batch_size=int(data_hps.val_bs),
         shuffle=False,
         drop_last=False,
         num_workers=4,
